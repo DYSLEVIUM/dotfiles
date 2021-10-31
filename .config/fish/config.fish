0cc0c2a4d904c22382bbf4728401caf96b66bc0a
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-10-31 17:20:50 +0545

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,18 @@
+set fish_greeting
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# default editor
+export EDITOR=nvim
+
+# aliases
+alias ls="ls --color=auto"
+alias grep="grep --color=auto"
+
+# starship
 starship init fish | source
+
+# neofetch
+neofetch
