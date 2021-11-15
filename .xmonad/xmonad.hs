ad403699d0cbdf4818e5cb7ab09b24832757bb3e
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-11-15 12:23:08 +0530

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -25,7 +25,7 @@ import qualified XMonad.StackSet as W
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal = "kitty"
+myTerminal = "konsole"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
