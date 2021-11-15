faac7de5d91e56f070f6fa632a3451a1c4b1e68f
DYSLEVIUM <pushpakantbehera@hotmail.com>
2021-11-15 15:56:03 +0530

    fixed polybar location

--- polybar/launch.sh
+++
@@ -1,7 +0,0 @@
-killall -q polybar
-
-# wait until the process is down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# launch bar1 and bar2
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
