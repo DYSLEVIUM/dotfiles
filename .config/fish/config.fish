613d6f19192e56a8672b217ec66926d3788cc56f
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-05-18 16:44:04 +0530

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,3 +18,11 @@ starship init fish | source
 
 # Running pfetch
 pfetch
+
+function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
+    if test "$argv" = !!
+    eval command sudo $history[1]
+else
+    command sudo $argv
+    end
+end
