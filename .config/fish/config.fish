eb18272d00a47fe388b3c8e88e0136d13575f478
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-06-05 22:16:00 +0530

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,34 +1,27 @@
 set fish_greeting
-fish_vi_key_bindings
+set fish_vi_key_bindings
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+# default editor
+set -x EDITOR nvim
 
-# Default editor
-export EDITOR=nvim
+# pfetch config
+set -x PF_ASCII arch
+set -x PF_COL1 1
+set -x PF_COL2 8
+set -x PF_COL3 3
 
-# Aliases
+# bat config
+set -x BAT_THEME gruvbox-dark
+
+# aliases
 alias ls='exa'
 alias ll='exa -la'
 alias tree='exa --tree'
 alias cat='bat -p'
 alias grep='grep --color=auto'
+alias docker='sudo docker'
 alias cc='clear'
 alias n='nvim'
-
-starship init fish | source
-
-# Running pfetch
-pfetch
-
-function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
-    if test "$argv" = !!
-    eval command sudo $history[1]
-else
-    command sudo $argv
-    end
-end
 
 function wrap_fzf_file
   fzf_file --preview "bat --style=numbers --color=always --line-range :500 {}"
@@ -37,4 +30,17 @@ end
 function fish_user_key_bindings
   bind \cr fzf_history
   bind \co wrap_fzf_file
+end
+
+starship init fish | source
+
+# running pfetch
+pfetch
+
+function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
+    if test "$argv" = !!
+    eval command sudo $history[1]
+else
+    command sudo $argv
+    end
 end
