96d042cafae2002eba929330ffe46f1a725507fc
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-07-01 08:32:57 +0530

    Update init.lua

--- .config/nvim_lua/lua/custom/plugins/init.lua
+++ .config/nvim_lua/lua/custom/plugins/init.lua
@@ -9,4 +9,39 @@ return {
       require "custom.plugins.null-ls"
     end,
   },
+
+   ["windwp/nvim-ts-autotag"] = {
+      filetypes = {
+        'html',
+        'javascript',
+        'typescript',
+        'javascriptreact',
+        'typescriptreact',
+        'svelte',
+        'vue',
+        'tsx',
+        'jsx',
+        'rescript',
+        'markdown',
+        'handlebars',
+        'hbs'
+      },
+      skip_tags = {
+        'br',
+        'embed',
+        'hr',
+        'img',
+        'input',
+        'link',
+        'meta',
+      },
+      after = "nvim-treesitter",
+      config = function()
+         local present, autotag = pcall(require, "nvim-ts-autotag")
+
+         if present then
+            autotag.setup()
+         end
+      end,
+   },
 }
