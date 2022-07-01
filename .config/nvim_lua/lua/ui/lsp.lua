9a2eb52d51db863847a09bf949e7d511550bc2e4
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-07-01 09:37:24 +0530

    Update lsp.lua

--- .config/nvim_lua/lua/ui/lsp.lua
+++ .config/nvim_lua/lua/ui/lsp.lua
@@ -14,7 +14,7 @@ vim.diagnostic.config {
    },
    signs = true,
    underline = true,
-   update_in_insert = false,
+   update_in_insert = true,
 }
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
