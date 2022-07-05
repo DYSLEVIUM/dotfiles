60237f56f4b3875b656a97e8b9d02a0fc52b11ec
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-07-06 03:14:24 +0530

    Update override.lua

--- .config/nvim_lua/lua/custom/override.lua
+++ .config/nvim_lua/lua/custom/override.lua
@@ -57,4 +57,14 @@ M.nvimtree = {
    },
 }
 
+M.luasnip = function()
+  local present, luasnip = pcall(require, "luasnip")
+
+   if not present then
+      return
+   end
+
+    require("luasnip.loaders.from_vscode").lazy_load({ paths = "./lua/custom/snippets" })
+end
+
 return M
