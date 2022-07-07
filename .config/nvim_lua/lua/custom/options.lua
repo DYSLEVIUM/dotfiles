02913b37d37494556ca0fe5efa5677e4c14966c6
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-07-07 19:10:50 +0530

    Create options.lua

---
+++ .config/nvim_lua/lua/custom/options.lua
@@ -0,0 +1,73 @@
+local opt = vim.opt
+
+opt.syntax = "enable"
+opt.conceallevel = 0
+opt.completeopt = { "menu", "menuone", "noselect" }
+opt.hidden = true
+opt.encoding = "utf-8"
+opt.pumheight = 10
+opt.fileencoding = "utf-8"
+opt.cmdheight = 1
+opt.wildmenu = true
+opt.lazyredraw = true
+opt.showmatch = true
+
+opt.hlsearch = true
+opt.incsearch = true
+
+opt.background = "dark"
+opt.showtabline = 1
+opt.timeoutlen = 500
+opt.formatoptions = "cro"
+opt.autochdir = true
+opt.shell = "fish"
+opt.showcmd = true
+
+opt.showmode = false
+
+opt.title = true
+opt.clipboard = "unnamedplus"
+opt.cursorline = true
+opt.wrap = true
+
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
+-- Indenting
+opt.autoindent = true
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.smarttab = true
+opt.tabstop = 2
+
+opt.fillchars = { eob = " " }
+opt.ignorecase = true
+opt.smartcase = true
+opt.mouse = "a"
+
+-- Numbers
+opt.number = true
+opt.numberwidth = 4
+opt.ruler = true
+opt.relativenumber = true
+
+-- disable nvim intro
+opt.shortmess:append "sI"
+
+opt.signcolumn = "yes"
+opt.splitbelow = true
+opt.splitright = true
+opt.termguicolors = true
+opt.timeoutlen = 400
+opt.undofile = true
+opt.swapfile = false
+opt.writebackup = false
+
+-- interval for writing swap file to disk, also used by gitsigns
+opt.updatetime = 250
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap:append "<>[]hl"
+
