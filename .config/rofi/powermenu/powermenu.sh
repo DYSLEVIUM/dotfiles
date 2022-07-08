f97f2ece66ba014badb37a95e016725e6b59e285
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-07-08 18:47:15 +0530

    Update powermenu.sh

--- .config/rofi/powermenu/powermenu.sh
+++ .config/rofi/powermenu/powermenu.sh
@@ -105,6 +105,8 @@ case $chosen in
 				openbox --exit
 			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
 				bspc quit
+			elif [[ "$DESKTOP_SESSION" == "herbstluftwm" ]]; then
+				herbstclient quit
 			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
 				i3-msg exit
 			fi
