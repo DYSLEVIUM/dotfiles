0bef418a58f0a1d7716ef00f43e782e673703082
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-10-19 12:09:00 +0530

    Update lspconfig.lua

--- .config/nvim/lua/custom/plugins/lspconfig.lua
+++ .config/nvim/lua/custom/plugins/lspconfig.lua
@@ -1,48 +1,51 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 local servers = {
-  "bashls",
-  "clangd",
-  "cssls",
-  "cssmodules_ls",
-  "emmet_ls",
-  "jsonls",
-  "html",
-  "dockerls",
-  "dotls",
-  "sqlls",
-  "eslint",
-  "graphql",
-  "tsserver",
-  "pyright",
-  "yamlls",
-  "bashls",
-  "cmake",
-  "tailwindcss",
-  "sumneko_lua",
-  "ltex",
-  "gopls",
+	"bashls",
+	"clangd",
+	"ccls",
+	"cssls",
+	"cssmodules_ls",
+	"emmet_ls",
+	"jsonls",
+	"html",
+	"dockerls",
+	"dotls",
+	"sqlls",
+	"eslint",
+	"graphql",
+	"tsserver",
+	"pyright",
+	"yamlls",
+	"bashls",
+	"cmake",
+	"tailwindcss",
+	"sumneko_lua",
+	"ltex",
+	"gopls",
 }
 
-local init_options = {}
 for _, lsp in ipairs(servers) do
-  if lsp == "clangd" then
-    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-    capabilities.textDocument.semanticHighlighting = true
-    capabilities.offsetEncoding = "utf-16"
-
-    init_options = {
-      clangdFileStatus = true,
-      usePlaceholders = true,
-      completeUnimported = true,
-      semanticHighlighting = true,
-    }
-  end
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    init_options = init_options,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
+
+-- clangd specific overrides
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+capabilities.textDocument.semanticHighlighting = true
+capabilities.offsetEncoding = "utf-16"
+
+lspconfig["clangd"].setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	init_options = {
+		clangdFileStatus = true,
+		usePlaceholders = true,
+		completeUnimported = true,
+		semanticHighlighting = true,
+	},
+})
