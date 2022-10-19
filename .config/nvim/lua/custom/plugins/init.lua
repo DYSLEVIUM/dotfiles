1fd34f45745318cfe19707da6fa7f78dd5498d1c
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-10-19 10:24:20 +0530

    Update init.lua

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -1,3 +1,4 @@
+-- TODO adjlaswjdl
 return {
   ["goolord/alpha-nvim"] = {
     disable = false,
@@ -102,9 +103,92 @@ return {
     end,
   },
 
+  ["folke/todo-comments.nvim"] = {
+    event = "BufRead",
+    config = function()
+      require("todo-comments").setup()
+    end,
+  },
+
+  ["folke/trouble.nvim"] = {
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        position = "bottom", -- position of the list can be: bottom, top, left, right
+      height = 10, -- height of the trouble list when position is top or bottom
+      width = 50, -- width of the list when position is left or right
+      icons = true, -- use devicons for filenames
+      mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+      fold_open = "", -- icon used for open folds
+      fold_closed = "", -- icon used for closed folds
+      group = true, -- group results by file
+      padding = true, -- add an extra new line on top of the list
+      action_keys = { -- key mappings for actions in the trouble list
+          -- map to {} to remove a mapping, for example:
+          -- close = {},
+          close = "q", -- close the list
+          cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+          refresh = "r", -- manually refresh
+          jump = {"<cr>", "<tab>"}, -- jump to the diagnostic or open / close folds
+          open_split = { "<c-x>" }, -- open buffer in new split
+          open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
+          open_tab = { "<c-t>" }, -- open buffer in new tab
+          jump_close = {"o"}, -- jump to the diagnostic and close the list
+          toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+          toggle_preview = "P", -- toggle auto_preview
+          hover = "K", -- opens a small popup with the full multiline message
+          preview = "p", -- preview the diagnostic location
+          close_folds = {"zM", "zm"}, -- close all folds
+          open_folds = {"zR", "zr"}, -- open all folds
+          toggle_fold = {"zA", "za"}, -- toggle fold of current file
+          previous = "k", -- previous item
+          next = "j" -- next item
+      },
+      indent_lines = true, -- add an indent guide below the fold icons
+      auto_open = false, -- automatically open the list when you have diagnostics
+      auto_close = false, -- automatically close the list when you have no diagnostics
+      auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+      auto_fold = false, -- automatically fold a file trouble list at creation
+      auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
+      signs = {
+          -- icons / text used for a diagnostic
+          error = "",
+          warning = "",
+          hint = "",
+          information = "",
+          other = "﫠"
+      },
+      use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+      }
+    end
+  },
+
   ["tpope/vim-surround"] = {},
 
+  ["tpope/vim-repeat"] = {},
+
   ["justinmk/vim-sneak"] = {},
+
+  ["p00f/nvim-ts-rainbow"] = {},
+
+  ["karb94/neoscroll.nvim"] = {
+    event = "WinScrolled",
+    config = function()
+      require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
+          '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+        hide_cursor = true, -- Hide cursor while scrolling
+        stop_eof = true, -- Stop at <EOF> when scrolling downwards
+        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+        respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil, -- Default easing function
+        pre_hook = nil, -- Function to run before the scrolling animation starts
+        post_hook = nil, -- Function to run after the scrolling animation ends
+      })
+    end
+  },
 
   ["phaazon/hop.nvim"] = {
     branch = "v1",
