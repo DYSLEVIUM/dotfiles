65b72b86ef01abc090f2a2f781496d67de76fbc0
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-10-24 11:24:25 +0530

    Update init.lua

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -242,9 +242,10 @@ return {
 				use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
 				respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
 				cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-				easing_function = nil, -- Default easing function
+				easing_function = "quintic", -- Default easing function
 				pre_hook = nil, -- Function to run before the scrolling animation starts
 				post_hook = nil, -- Function to run after the scrolling animation ends
+				performance_mode = false,
 			})
 		end,
 	},
