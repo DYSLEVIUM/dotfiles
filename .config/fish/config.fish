aa9c0e82bc4fe718d30634af837b86eadb97dc20
Pushpakant Behera <pushpakantbehera@hotmail.com>
2023-05-23 13:42:08 +0530

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,25 +1,27 @@
 set fish_greeting
 fish_vi_key_bindings
+# fish_default_key_bindings
+
+theme_gruvbox 'dark' 'medium'
 
 # default editor
 set -x EDITOR nvim
 
 # pfetch config
-set -x PF_ASCII arch
-set -x PF_COL1 1
+set -x PF_COL1 3
 set -x PF_COL2 8
-set -x PF_COL3 3
+set -x PF_COL3 2
 
 # bat config
 set -x BAT_THEME gruvbox-dark
 
 # aliases
-alias ls='exa'
-alias ll='exa -la'
-alias tree='exa --tree'
-alias cat='bat -p'
+alias ls='/opt/homebrew/bin/exa'
+alias ll='/opt/homebrew/bin/exa -la'
+alias tree='/opt/homebrew/bin/exa --tree'
+alias cat='/opt/homebrew/bin/bat'
 alias grep='grep --color=auto'
-alias docker='sudo docker'
+alias docker='docker'
 alias cc='clear'
 alias n='nvim'
 
@@ -30,6 +32,7 @@ end
 function fish_user_key_bindings
   bind \cr fzf_history
   bind \co wrap_fzf_file
+    bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 end
 
 starship init fish | source
@@ -48,3 +51,10 @@ end
 # Bun
 # set -Ux BUN_INSTALL "~/.bun"
 # set -px --path PATH "~/.bun/bin"
+
+set -x HOMEBREW_NO_INSTALL_CLEANUP TRUE
+
+# pnpm
+set -gx PNPM_HOME "/Users/pushpakantbehera/Library/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
