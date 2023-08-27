339ae8038cc941c2f05088be386c9e8e621bcc91
Pushpakant Behera <pushpakant.b.behera@oracle.com>
2023-08-28 02:49:06 +0530

    removed unnecessary plugins

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -161,177 +161,104 @@ M.qualityOfLife = {
     },
 }
 
-M.tabufline = {
-    plugin = true,
-
-    n = {
-        -- cycle through buffers
-        ["<tab>"] = { function()
-            -- !changed
-            -- require("nvchad_ui.tabufline").tabuflineNext()
-        end, "Goto next buffer" },
-
-        ["<S-tab>"] = { function()
-            -- !changed
-            -- require("nvchad_ui.tabufline").tabuflinePrev()
-        end, "Goto prev buffer" },
-
-        -- close buffer + hide terminal buffer
-        ["<leader>x"] = { function()
-            -- !changed
-            -- require("nvchad_ui.tabufline").close_buffer()
-        end, "Close buffer" }
-    }
-}
-
--- !changed
--- M.tagbar = {
+-- M.lspconfig = {
 --     plugin = true,
-
+--
+--     -- See `<cmd> :help vim.lsp.*` for documentation on any of the below functions
+--
 --     n = {
---         ["<F8>"] = { "<cmd> TagbarToggle <CR>", "Toggle tagbar" }
+--         ["gD"] = { function()
+--             vim.lsp.buf.declaration()
+--         end, "LSP declaration" },
+--
+--         ["gd"] = { function()
+--             vim.lsp.buf.definition()
+--         end, "LSP definition" },
+--
+--         ["K"] = { function()
+--             vim.lsp.buf.hover()
+--         end, "LSP hover" },
+--
+--         ["gi"] = { function()
+--             vim.lsp.buf.implementation()
+--         end, "LSP implementation" },
+--
+--         ["<leader>ls"] = { function()
+--             vim.lsp.buf.signature_help()
+--         end, "LSP signature help" },
+--
+--         ["<leader>D"] = { function()
+--             vim.lsp.buf.type_definition()
+--         end, "LSP definition type" },
+--
+--         --! changed
+--         -- ["<leader>ra"] = {function()
+--         --     require("nvchad_ui.renamer").open()
+--         -- end, "LSP rename"},
+--
+--         ["<leader>ca"] = { function()
+--             vim.lsp.buf.code_action()
+--         end, "LSP code action" },
+--
+--         ["gr"] = { function()
+--             vim.lsp.buf.references()
+--         end, "LSP references" },
+--
+--         ["<leader>f"] = { function()
+--             vim.diagnostic.open_float {
+--                 border = "rounded"
+--             }
+--         end, "Floating diagnostic" },
+--
+--         ["[d"] = { function()
+--             vim.diagnostic.goto_prev {
+--                 float = {
+--                     border = "rounded"
+--                 }
+--             }
+--         end, "Goto prev" },
+--
+--         ["]d"] = { function()
+--             vim.diagnostic.goto_next {
+--                 float = {
+--                     border = "rounded"
+--                 }
+--             }
+--         end, "Goto next" },
+--
+--         ["<leader>q"] = { function()
+--             vim.diagnostic.setloclist()
+--         end, "Diagnostic setloclist" },
+--
+--         ["<leader>wa"] = { function()
+--             vim.lsp.buf.add_workspace_folder()
+--         end, "Add workspace folder" },
+--
+--         ["<leader>wr"] = { function()
+--             vim.lsp.buf.remove_workspace_folder()
+--         end, "Remove workspace folder" },
+--
+--         ["<leader>wl"] = { function()
+--             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--         end, "List workspace folders" }
 --     }
 -- }
-
-M.lspconfig = {
-    plugin = true,
-
-    -- See `<cmd> :help vim.lsp.*` for documentation on any of the below functions
-
-    n = {
-        ["gD"] = { function()
-            vim.lsp.buf.declaration()
-        end, "LSP declaration" },
-
-        ["gd"] = { function()
-            vim.lsp.buf.definition()
-        end, "LSP definition" },
-
-        ["K"] = { function()
-            vim.lsp.buf.hover()
-        end, "LSP hover" },
-
-        ["gi"] = { function()
-            vim.lsp.buf.implementation()
-        end, "LSP implementation" },
-
-        ["<leader>ls"] = { function()
-            vim.lsp.buf.signature_help()
-        end, "LSP signature help" },
-
-        ["<leader>D"] = { function()
-            vim.lsp.buf.type_definition()
-        end, "LSP definition type" },
-
-        --! changed
-        -- ["<leader>ra"] = {function()
-        --     require("nvchad_ui.renamer").open()
-        -- end, "LSP rename"},
-
-        ["<leader>ca"] = { function()
-            vim.lsp.buf.code_action()
-        end, "LSP code action" },
-
-        ["gr"] = { function()
-            vim.lsp.buf.references()
-        end, "LSP references" },
-
-        ["<leader>f"] = { function()
-            vim.diagnostic.open_float {
-                border = "rounded"
-            }
-        end, "Floating diagnostic" },
-
-        ["[d"] = { function()
-            vim.diagnostic.goto_prev {
-                float = {
-                    border = "rounded"
-                }
-            }
-        end, "Goto prev" },
-
-        ["]d"] = { function()
-            vim.diagnostic.goto_next {
-                float = {
-                    border = "rounded"
-                }
-            }
-        end, "Goto next" },
-
-        ["<leader>q"] = { function()
-            vim.diagnostic.setloclist()
-        end, "Diagnostic setloclist" },
-
-        ["<leader>wa"] = { function()
-            vim.lsp.buf.add_workspace_folder()
-        end, "Add workspace folder" },
-
-        ["<leader>wr"] = { function()
-            vim.lsp.buf.remove_workspace_folder()
-        end, "Remove workspace folder" },
-
-        ["<leader>wl"] = { function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, "List workspace folders" }
-    }
-}
-
-M.telescope = {
-    plugin = true,
-
-    n = {
-        -- find
-        ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "Find files" },
-        ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
-        ["<leader>fw"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
-        ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Find buffers" },
-        ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Help page" },
-        ["<leader>fo"] = { "<cmd> Telescope oldfiles <CR>", "Find oldfiles" },
-        ["<leader>fz"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "Find in current buffer" },
-
-        -- git
-        ["<leader>cm"] = { "<cmd> Telescope git_commits <CR>", "Git commits" },
-        ["<leader>gt"] = { "<cmd> Telescope git_status <CR>", "Git status" },
-
-        -- pick a hidden term
-        -- ["<leader>pt"] = {"<cmd> Telescope terms <CR>", "Pick hidden term"},
-
-        -- theme switcher
-        -- ["<leader>th"] = {"<cmd> Telescope themes <CR>", "Nvchad themes"},
-
-        ["<leader>ma"] = { "<cmd> Telescope marks <CR>", "telescope bookmarks" }
-    }
-}
-
-M.whichkey = {
-    plugin = true,
-
-    n = {
-        ["<leader>wk"] = { function()
-            vim.cmd "WhichKey"
-        end, "Which-key all keymaps" },
-        ["<leader>wK"] = { function()
-            local input = vim.fn.input "WhichKey: "
-            vim.cmd("WhichKey " .. input)
-        end, "Which-key query lookup" }
-    }
-}
-
-M.blankline = {
-    plugin = true,
-
-    n = {
-        ["<leader>cc"] = { function()
-            local ok, start = require("indent_blankline.utils").get_current_context(vim.g
-                .indent_blankline_context_patterns,
-                vim.g.indent_blankline_use_treesitter_scope)
-
-            if ok then
-                vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { start, 0 })
-                vim.cmd [[normal! _]]
-            end
-        end, "Jump to current context" }
-    }
-}
+--
+-- M.blankline = {
+--     plugin = true,
+--
+--     n = {
+--         ["<leader>cc"] = { function()
+--             local ok, start = require("indent_blankline.utils").get_current_context(vim.g
+--                 .indent_blankline_context_patterns,
+--                 vim.g.indent_blankline_use_treesitter_scope)
+--
+--             if ok then
+--                 vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { start, 0 })
+--                 vim.cmd [[normal! _]]
+--             end
+--         end, "Jump to current context" }
+--     }
+-- }
 
 return M
