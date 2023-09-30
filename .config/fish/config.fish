7e7787dd1630ea6456e74984262becbb195645f7
Pushpakant Behera <pushpakantbehera@hotmail.com>
2023-09-30 06:02:47 +0530

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,10 @@
 set fish_greeting
+set -gx COLORTERM truecolor
+
 fish_vi_key_bindings
 # fish_default_key_bindings
 
-theme_gruvbox 'dark' 'medium'
+theme_gruvbox dark medium
 
 # default editor
 set -x EDITOR nvim
@@ -15,7 +17,10 @@ set -x PF_COL3 2
 # bat config
 set -x BAT_THEME gruvbox-dark
 
-# aliases
+# tmux config with fish plugin
+set -x fish_tmux_config $HOME/.config/tmux/tmux.conf
+set -x fish_tmux_default_session_name default
+
 alias ls='exa'
 alias ll='exa -laF --icons'
 alias tree='exa --tree'
@@ -32,31 +37,28 @@ alias ...='cd ../..'
 alias eip='curl ifconf.me'
 
 function mkcd --description "Create directory and change to that path"
-    command mkdir -p $argv; cd $argv
+    command mkdir -p $argv
+    cd $argv
 end
 
 function wrap_fzf_file
-  fzf_file --preview "bat --style=numbers --color=always --line-range :500 {}"
+    fzf_file --preview "bat --style=numbers --color=always --line-range :500 {}"
 end
 
 function fish_user_key_bindings
-  bind \cr fzf_history
-  bind \co wrap_fzf_file
-  bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+    bind \cr fzf_history
+    bind \co wrap_fzf_file
+    bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 end
 
 function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
     if test "$argv" = !!
-    eval command sudo $history[1]
-else
-    command sudo $argv
+        eval command sudo $history[1]
+    else
+        command sudo $argv
     end
 end
 
+# startup
 starship init fish | source
-
-# running pfetch
 pfetch
-
-	tmux new
-end
