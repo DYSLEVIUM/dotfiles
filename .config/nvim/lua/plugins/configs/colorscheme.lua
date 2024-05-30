8c0b92d5e4aa4d5ce508776d3802e5d564b79eb3
Pushpakant Behera <pushpakantbehera@hotmail.com>
2024-05-30 22:08:37 +0530

    Update colorscheme.lua

--- .config/nvim/lua/plugins/configs/colorscheme.lua
+++ .config/nvim/lua/plugins/configs/colorscheme.lua
@@ -2,10 +2,14 @@ return {
     "ellisonleao/gruvbox.nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-        transparent_mode = true,
-        contrast = "hard",
-    },
+    opts = function ()
+        local transparency = require("core.utils").load_config().ui.transparency
+
+        return {
+            transparent_mode = transparency,
+            contrast = "hard",
+        }
+    end,
     config = function(_, opts)
         require("gruvbox").setup(opts)
         vim.cmd([[colorscheme gruvbox]])
