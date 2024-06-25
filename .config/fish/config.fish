e20fa1761ee6a64baf2a6e3423105a93237fb6fe
Pushpakant Behera <pushpakantbehera@hotmail.com>
2024-06-26 03:01:46 +0530

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,38 +18,90 @@ set -x PF_COL3 2
 set -x BAT_THEME gruvbox-dark
 set -x K9S_CONFIG_DIR ~/.config/k9s
 
-alias exa='exa --group-directories-first'
-alias ls='exa'
+alias ls='exa --group-directories-first'
 alias ll='exa -laF --icons'
 alias tree='exa --tree'
 alias cat='bat'
 alias grep='grep --color=auto'
 alias cc='clear'
-alias n='nvim'
 alias cp='rsync -ivP'
 alias rm='rm -i'
 alias mkdir='mkdir -p'
 alias ..='cd ..'
 alias ...='cd ../..'
 alias eip='curl ifconf.me'
-alias p='pnpm'
-alias pm='podman'
-alias k='kubectl'
-alias tf='terraform'
 
-# git alisaes
-alias ga="git add"
-alias gs="git status"
-alias gm="git merge"
-alias gcm="git commit -m"
-alias gch="git checkout"
-alias gchb="git checkout -b"
-alias gb="git branch"
-alias gba="git branch -av"
-alias gbd="git branch -d"
-alias gp="git push"
-alias gpo="git push origin"
+# function aliases
+function n --wraps nvim
+    nvim $argv
+end
 
+function p --wraps pnpm
+    pnpm $argv
+end
+
+function pm --wraps podman
+    podman $argv
+end
+
+function k --wraps kubectl
+    kubectl $argv
+end
+
+function tf --wraps terraform
+    terraform $argv
+end
+
+## git
+function ga --wraps git
+    git add $argv
+end
+
+function gs --wraps git
+    git status $argv
+end
+
+function gm --wraps git
+    git merge $argv
+end
+
+function gcm --wraps git
+    git commit -m $argv
+end
+
+function gch --wraps git
+    git checkout $argv
+end
+
+function gchb --wraps git
+    git checkout -b $argv
+end
+
+function gb --wraps git
+    git branch $argv
+end
+
+function gba --wraps git
+    git branch -av $argv
+end
+
+function gbd --wraps git
+    git branch -d $argv
+end
+
+function gp --wraps git
+    git push $argv
+end
+
+function gpo --wraps git
+    git push origin $argv
+end
+
+function gch --wraps git
+    git checkout $argv
+end
+
+# custom functions
 function mkcd --description "Create directory and change to that path"
     command mkdir -p $argv
     cd $argv
@@ -89,8 +141,15 @@ set -gx PNPM_HOME /Users/pushpakantbehera/Library/pnpm
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
-# pnpm end
 
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+set --export --prepend PATH "/Users/pushpakantbehera/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
